5402a86fc22549e71d847a1e6196d7de7e01d460
Jassu123 <siva.nadupuru@gmail.com>
2023-03-27 15:39:40 +0200

    Feat/add readme instructions (#4)
    * feat: added reame more instruction and swagger for getbyname and peronal number api
    
    * fix: removed unused code

--- src/main/java/com/telia/repository/UserRepository.java
+++ src/main/java/com/telia/repository/UserRepository.java
@@ -2,17 +2,12 @@ package com.telia.repository;
 import com.telia.entity.User;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByPersonalNumber(Long personalNumber);
-    List<User> findByFullNameContainingAndPersonalNumber(
-            String name, Long personalNumber, Sort sort);
-
     List<User> findByFullNameAndPersonalNumber(String name, Long personalNumber, Sort by);
-
     List<User> findByFullName(String name, Sort by);
 }
